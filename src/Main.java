872d1bd4803f538aeb38f11ea88106b6ba2310b5
AlexKrylov <krylofffff93@gmail.com>
2023-01-30 22:04:04 +0300

    another solution

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
 import java.sql.ClientInfoStatus;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
+import java.util.function.BiConsumer;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main {
@@ -17,6 +20,8 @@ public class Main {
         //Task2
         getCount(numbers);
 
+        getMinMax(numbers.stream(), Comparator.comparingInt(x -> x), (x, y) -> System.out.println("Min:"+x + " Max: " + y));
+
     }
 
     public static void findMinMax(List<Integer> list) {
@@ -26,6 +31,20 @@ public class Main {
 
     public static void getCount(List<Integer> list) {
         System.out.println("В листе " + list.stream().filter(i -> i % 2 == 0).count() + " четных значения");
+    }
+
+    public static <T> void getMinMax(Stream<? extends T> stream, Comparator<? super T> order, BiConsumer<? super T, ? super T> minMaxConsumer) {
+        List<T> list = new ArrayList<>();
+        T min = null;
+        T max = null;
+        list = stream.sorted(order).collect(Collectors.toList());
+        if (list.size() != 0) {
+            min = list.get(0);
+            max = list.get(list.size() - 1);
+        }
+        minMaxConsumer.accept(min, max);
+
+
     }
 }
 
